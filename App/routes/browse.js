9a55cb90320be581b45541f37e012a5b84ee9b4b
Jhocx <jhocx@u.nus.edu>
2019-03-29 15:49:05 +0800

    Create browse.js and browse.ejs
    browse page now displays table restaurants.

--- App/routes/browse.js
+++ App/routes/browse.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const { Pool } = require('pg')
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL
+});
+
+
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	/* SQL Query */
+	var sql_query = 'SELECT * FROM ' + "restaurants"
+	pool.query(sql_query, (err, data) => {
+		if (err) {
+			res.render('error', {message: "Restaurant table not found", error: {status: "", stack: ""}})
+		} else {
+			res.render('browse', { title: 'View Table', data: data.rows, fields: data.fields, test: 123 });
+		}
+	});
 });
 
 module.exports = router;
