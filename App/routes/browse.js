c4616dca46588e0519d1fc75f4bc0fda08cf643a
Jhocx <jhocx@u.nus.edu>
2019-04-01 01:20:35 +0800

    Implement search by rname.

--- App/routes/browse.js
+++ App/routes/browse.js
@@ -10,12 +10,25 @@ const pool = new Pool({
 
 router.get('/', function(req, res, next) {
 	/* SQL Query */
-	var sql_query = 'SELECT * FROM ' + "restaurants"
+	var sql_query = 'SELECT * FROM restaurants'
 	pool.query(sql_query, (err, data) => {
 		if (err) {
 			res.render('error', {message: "Restaurant table not found", error: {status: "", stack: ""}})
 		} else {
-			res.render('browse', { title: 'View Table', data: data.rows, fields: data.fields, test: 123 });
+			res.render('browse', { title: 'View Table', data: data.rows, fields: data.fields});
+		}
+	});
+});
+
+router.post('/', function(req, res, next) {
+    /* SQL Query */
+    var s_rName = req.body.s_rName;
+	var rName_query = "SELECT * FROM restaurants r where r.rname = '" + s_rName + "';"
+	pool.query(rName_query, (err, data) => {
+		if (err) {
+			res.render('error', {message: "Restaurant table not found", error: {status: "", stack: ""}})
+		} else {
+			res.render('browse', { title: 'View Table', data: data.rows, fields: data.fields});
 		}
 	});
 });
