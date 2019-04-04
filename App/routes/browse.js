cb188b9be77bc55bc3677a34b155052099db0c91
Jhocx <jhocx@u.nus.edu>
2019-04-04 12:48:54 +0800

    Add search by tag (case insensitive and exact). Change search by rname and rlocation to not need to be exact.

--- App/routes/browse.js
+++ App/routes/browse.js
@@ -10,7 +10,7 @@ const pool = new Pool({
 
 router.get('/', function(req, res, next) {
 	/* SQL Query */
-	var sql_query = 'SELECT * FROM restaurants'
+	var sql_query = 'SELECT rname as "Name", rLocation as "Location", rrating as "Rating", opentime as "Opening Time", closetime as "Closing Time" FROM restaurants'
 	pool.query(sql_query, (err, data) => {
 		if (err) {
 			res.render('error', {message: "Restaurant table not found", error: {status: "", stack: ""}})
@@ -29,12 +29,14 @@ router.post('/', function(req, res, next) {
     var search_rRating = req.body.search_rRating;
     var search_rLocation = req.body.search_rLocation.toLowerCase();
     search_rLocation = search_rLocation.toLowerCase();
+    var search_rTag = req.body.search_rTag;
+    search_rTag = search_rTag.toLowerCase();
 
-	var search_query = "SELECT * FROM restaurants r"
-
+	var search_query = 'SELECT distinct rname as "Name", rLocation as "Location", rrating as "Rating", opentime as "Opening Time", closetime as "Closing Time" '
+	 + ' FROM (restaurants r inner join cuisinetags ct ON r.rid = ct.rid) as r'
 	if (search_rName === "") {
 	} else {
-	    search_query = search_query + " where LOWER(r.rname) = '" + search_rName + "'"
+	    search_query = search_query + " where LOWER(r.rname) like '%" + search_rName + "%'"
 	    num_conditions++
 	}
 
@@ -59,6 +61,19 @@ router.post('/', function(req, res, next) {
             num_conditions++
         }
 	}
+
+	if (search_rTag === "") {
+	} else {
+	    if (num_conditions == 0) {
+        	search_query = search_query + " where LOWER(r.tag) = '" + search_rTag + "'"
+        	num_conditions++
+        } else {
+            search_query = search_query + " and LOWER(r.tag) = '" + search_rTag + "'"
+            num_conditions++
+        }
+	}
+
+	//search_query = search_query + ' group by rname'
 
 	pool.query(search_query, (err, data) => {
 		if (err) {
