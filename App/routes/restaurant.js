8bc9e16933f805b3451d0f1a150e423d30f211a7
yogtew <reachyogusvi@gmail.com>
2019-04-04 12:33:09 +0800

    Add gitignore

--- App/routes/restaurant.js
+++ App/routes/restaurant.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool } = require('pg')
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var sql_query = 'select * from restaurants'
+  pool.query(sql_query, (err, data) => {
+  		if (err) {
+  			res.render('error', {message: "Table \"" + req.query.table + "\" not found", error: {status: "", stack: ""}})
+  		} else {
+  			res.render('restaurant', { title: 'restaurant', data: data.rows, fields: data.fields});
+  		}
+  	});
 });
 
 module.exports = router;
