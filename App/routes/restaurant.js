ba767afa43f4ad8fcff379c2781c861365068f6d
yogtew <reachyogusvi@gmail.com>
2019-04-10 20:30:32 +0800

    Add reservation date box (not working yet)

--- App/routes/restaurant.js
+++ App/routes/restaurant.js
@@ -6,12 +6,15 @@ const pool = new Pool({
 	connectionString: process.env.DATABASE_URL
 });
 
-/* GET users listing. */
+/* GET all the restaurants */
 router.get('/', function(req, res, next) {
+  // i want it to display the specific restaurant page when user selects a restaurant from like the dashboard
   var sql_query = 'select * from restaurants'
   pool.query(sql_query, (err, data) => {
+        console.log(data);
   		if (err) {
-  			res.render('error', {message: "Table \"" + req.query.table + "\" not found", error: {status: "", stack: ""}})
+  			res.render('error', {message: "Table \"" + req.query.table + "\" not found",
+  			                     error: {status: "", stack: ""}})
   		} else {
   			res.render('restaurant', { title: 'restaurant', data: data.rows, fields: data.fields});
   		}
