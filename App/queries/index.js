84fc672926bc8b461b1f90dcbbebae6282e7b394
yogtew <reachyogusvi@gmail.com>
2019-04-12 19:41:47 +0800

    display avg reservations for restaurant

--- App/queries/index.js
+++ App/queries/index.js
@@ -9,6 +9,6 @@ module.exports = {
     clearFoodPrefsForUser: "delete from FoodPreferences where custid = $1",
     clearLocPrefsForUser: "delete from LocationPreferences where custid = $1",
     setFoodPrefsForUser: "insert into FoodPreferences(custid, tagid) values ",
-    setLocPrefsForUser: "insert into LocationPreferences(custid, plocation) values "
-    getAvgReservations = "with ReservationData as (select B.rname as resName, cast(extract(month from A.restime) as int) as nameOfMonth, count(*) as numOfReservations from reserves A natural join restaurant B where rid = $1 group by (extract(month from A.restime), B.rid)) select RD.resName, RD.nameOfMonth, Avg(RD.numOfReservations) over W as avgReservations from ReservationData RD window W as(partition by RD.resName order by RD.nameOfMonth rows 2 preceding)"
+    setLocPrefsForUser: "insert into LocationPreferences(custid, plocation) values ",
+    getAvgReservations: "with ReservationData as (select B.rname as resName, cast(extract(month from A.restime) as int) as nameOfMonth, cast(count(*) as int) as numOfReservations from reserves A natural join restaurant B where rid = $1 group by (extract(month from A.restime), B.rid)) select RD.resName, RD.nameOfMonth, Avg(RD.numOfReservations) over W as avgReservations from ReservationData RD window W as(partition by RD.resName order by RD.nameOfMonth rows 2 preceding)"
 }
